ada7676bd42f6873c16b435435b63c7d7ce798ae
Maros Zatko <hacxman@gmail.com>
2017-05-29 01:36:39 +0200

    before refactor SSD1306 driver

--- test/BlinkTest.hs
+++ test/BlinkTest.hs
@@ -5,12 +5,12 @@ import Ivory.Tower.Config
 import Ivory.Tower.Options
 import Ivory.OS.FreeRTOS.Tower.STM32
 
-import Hello.Tests.Platforms
-import Hello.Tests.Blink (app)
+import Clock.Platforms
+import Clock.Blink (app)
 
 main :: IO ()
 main = compileTowerSTM32FreeRTOS testplatform_stm32 p $
-  app testplatform_leds
+  app testplatform_clockconfig testplatform_leds testplatform_i2c testplatform_uart
   where
   p :: TOpts -> IO TestPlatform
   p topts = getConfig topts testPlatformParser
